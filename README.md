840ad04c8fb9461492667d35ac854f62aad73ed0
Dayne Daylong <dalexad@gmail.com>
2019-12-18 13:14:07 -0800

    Initial creation

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# Lab-11-14
-Completed by Dayne
+# Book-Lab
+
+**Author**: Dayne Daylong
+**Version**: 1.3.0 (increment the patch/fix version number if you make more commits past your first submission)
+
+## Overview
+Given that a user opens the application in the browser
+When the user navigates to the home page
+Then the index should load without a flash of unstyled content (FOUC)
+
+## Getting Started
+visit the repository or heroku site 
+
+## Architecture
+Express, EJS, Heroku, Superagent and PG
+
+## Change Log
+
+## Credits and Collaborations
+
+
+
+-->
---
+++ server.js
@@ -0,0 +1,70 @@
+'use strict';
+
+
+// global const
+const express = require('express');
+const superagent = require('superagent');
+const app = express();
+
+// const books = [];
+
+// app.'s
+app.set('view engine', 'ejs');
+app.use(express.static('styles'));
+
+
+// function to handle errors
+function errors(error, response) {
+  console.error(error);
+  response.render('error');
+}
+
+console.log(errors);
+
+
+// constructor
+// function BookObject(book) {
+//   this.title = book.volumeInfo.title
+//   this.authors = book.volumeInfo.authors
+//   this.description = book.volumeInfo.description
+//   console.log(this)
+// }
+
+
+// When page is inittially displayed:
+app.get('/', (req, res) => {
+  superagent.get(`https://www.googleapis.com/books/v1/volumes/?q=${'star wars'}`)
+    .then(book => {
+      // console.log(book);
+      res.render('index', { books: book.body.items });
+    });
+
+});
+
+
+// If search bar button is hit:
+app.post('/', (req, res) => {
+  superagent.get(`https://www.googleapis.com/books/v1/volumes/?q=${'star trek'}`)
+    .then(book => {
+      // console.log(book);
+
+
+      // constructor function
+      // function BookObject(title, author, description, img) {
+      //   this.title = book.volumeInfo.title
+      //   this.authors = book.volumeInfo.authors
+      //   this.description = book.volumeInfo.description
+      //   this.img = 
+      //   console.log(this)
+      // }
+
+
+      res.render('show', { books: book.body.items });
+    });
+
+});
+
+
+const PORT = process.env.PORT || 3099
+app.listen(PORT, () => console.log(`Port ${PORT} for the win!`));
+
